7126e4cc277e171e7eef624152d7f909ba9d18f6
carrieta14 <carrieta14@cuc.edu.co>
2023-03-10 22:35:13 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+try {
+  document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('index').addEventListener('click', function () {
+      window.location.href = 'index.html';
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('servicios').addEventListener('click', function () {
+      window.location.href = 'services.html';
+    });
+  });
+  document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('contacto').addEventListener('click', function () {
+      window.location.href = 'contact.html';
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('about').addEventListener('click', function () {
+      window.location.href = 'about_us.html';
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('index_2').addEventListener('click', function () {
+      window.location.href = 'index.html';
+    });
+  });
+}catch{
+
+}
+
+
+
+
+
